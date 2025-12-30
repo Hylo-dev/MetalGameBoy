a47216ac03518f5099e5db74ffe7ca73b29e961c
Eliomar Rodriguez <104672885+eliorodr2104@users.noreply.github.com>
2025-12-30 14:35:07 +0100

    Initial Commit

---
+++ MetalGameBoy/MetalGameBoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalGameBoyApp.swift
+//  MetalGameBoy
+//
+//  Created by Eliomar Alejandro Rodriguez Ferrer on 30/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MetalGameBoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
